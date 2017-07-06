440555c7cce86b612a0dbb9a9ccafdc0fd3291e2
Piasy <xz4215@gmail.com>
2017-07-06 22:38:13 +0800

    set up

---
+++ app/src/main/java/com/github/piasy/openglestutorial_android/MainActivity.java
@@ -0,0 +1,70 @@
+package com.github.piasy.openglestutorial_android;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GLSurfaceView mGLSurfaceView;
+    private MyRenderer mRenderer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (!Utils.supportGlEs20(this)) {
+            Toast.makeText(this, "GLES 2.0 not supported!", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+
+        mGLSurfaceView = (GLSurfaceView) findViewById(R.id.surface);
+
+        mGLSurfaceView.setEGLContextClientVersion(2);
+        mRenderer = new MyRenderer();
+        mGLSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        mGLSurfaceView.setRenderer(mRenderer);
+        mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    private static class MyRenderer implements GLSurfaceView.Renderer {
+
+        @Override
+        public void onSurfaceCreated(GL10 unused, EGLConfig config) {
+            GLES20.glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 unused, int width, int height) {
+            GLES20.glViewport(0, 0, width, height);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 unused) {
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+        }
+    }
+}
---
+++ app/src/main/java/com/github/piasy/openglestutorial_android/Utils.java
@@ -0,0 +1,60 @@
+package com.github.piasy.openglestutorial_android;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.opengl.GLES20;
+import android.os.Environment;
+import android.util.Log;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by Piasy{github.com/Piasy} on 6/7/16.
+ */
+public class Utils {
+
+    public static boolean supportGlEs20(Activity activity) {
+        ActivityManager activityManager = (ActivityManager) activity.getSystemService(
+                Context.ACTIVITY_SERVICE);
+        return activityManager.getDeviceConfigurationInfo().reqGlEsVersion >= 0x20000;
+    }
+
+    static void sendImage(int width, int height) {
+        ByteBuffer rgbaBuf = ByteBuffer.allocateDirect(width * height * 4);
+        rgbaBuf.position(0);
+        long start = System.nanoTime();
+        GLES20.glReadPixels(0, 0, width, height, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE,
+                rgbaBuf);
+        long end = System.nanoTime();
+        Log.d("TryOpenGL", "glReadPixels: " + (end - start));
+        saveRgb2Bitmap(rgbaBuf, Environment.getExternalStorageDirectory().getAbsolutePath()
+                                + "/gl_dump_" + width + "_" + height + ".png", width, height);
+    }
+
+    static void saveRgb2Bitmap(Buffer buf, String filename, int width, int height) {
+        Log.d("TryOpenGL", "Creating " + filename);
+        BufferedOutputStream bos = null;
+        try {
+            bos = new BufferedOutputStream(new FileOutputStream(filename));
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            bmp.copyPixelsFromBuffer(buf);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, bos);
+            bmp.recycle();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bos != null) {
+                try {
+                    bos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
