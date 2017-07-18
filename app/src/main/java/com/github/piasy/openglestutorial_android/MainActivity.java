624d0a85f88158f2eb29f33952e73a04491ebb6f
Piasy <xz4215@gmail.com>
2017-07-18 22:28:31 +0800

    Open-gl-es-android-2-part-2

--- app/src/main/java/com/github/piasy/openglestutorial_android/MainActivity.java
+++ app/src/main/java/com/github/piasy/openglestutorial_android/MainActivity.java
@@ -1,7 +1,11 @@
 package com.github.piasy.openglestutorial_android;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLUtils;
 import android.opengl.Matrix;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,12 +13,14 @@ import android.widget.Toast;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends AppCompatActivity {
 
     private GLSurfaceView mGLSurfaceView;
+    private MyRenderer mRenderer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
         mGLSurfaceView.setEGLContextClientVersion(2);
         mGLSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-        mGLSurfaceView.setRenderer(new MyRenderer());
+        mRenderer = new MyRenderer(this);
+        mGLSurfaceView.setRenderer(mRenderer);
         mGLSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 
@@ -47,38 +54,79 @@ public class MainActivity extends AppCompatActivity {
         mGLSurfaceView.onResume();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRenderer.destroy();
+    }
+
     private static class MyRenderer implements GLSurfaceView.Renderer {
 
         private static final String VERTEX_SHADER =
-                "attribute vec4 vPosition;\n"
-                + "uniform mat4 uMVPMatrix;\n"
-                + "void main() {\n"
-                + "  gl_Position = uMVPMatrix * vPosition;\n"
-                + "}";
+                "uniform mat4 uMVPMatrix;" +
+                "attribute vec4 vPosition;" +
+                "attribute vec2 a_texCoord;" +
+                "varying vec2 v_texCoord;" +
+                "void main() {" +
+                "  gl_Position = uMVPMatrix * vPosition;" +
+                "  v_texCoord = a_texCoord;" +
+                "}";
         private static final String FRAGMENT_SHADER =
-                "precision mediump float;\n"
-                + "void main() {\n"
-                + "  gl_FragColor = vec4(0.5, 0, 0, 1);\n"
-                + "}";
+                "precision mediump float;" +
+                "varying vec2 v_texCoord;" +
+                "uniform sampler2D s_texture;" +
+                "void main() {" +
+                "  gl_FragColor = texture2D(s_texture, v_texCoord);" +
+                "}";
+
         private static final float[] VERTEX = {   // in counterclockwise order:
-                0, 1, 0,  // top
-                -0.5f, -1, 0,  // bottom left
+                1, 1, 0,   // top right
+                -1, 1, 0,  // top left
+                -1, -1, 0, // bottom left
                 1, -1, 0,  // bottom right
         };
+        private static final short[] VERTEX_INDEX = {
+                0, 1, 2, 0, 2, 3
+        };
+        private static final float[] TEX_VERTEX = {   // in clockwise order:
+                0.5f, 0,  // bottom right
+                0, 0,  // bottom left
+                0, 0.5f,  // top left
+                0.5f, 0.5f,  // top right
+        };
 
+        private final Context mContext;
         private final FloatBuffer mVertexBuffer;
+        private final FloatBuffer mTexVertexBuffer;
+        private final ShortBuffer mVertexIndexBuffer;
         private final float[] mMVPMatrix = new float[16];
 
         private int mProgram;
         private int mPositionHandle;
         private int mMatrixHandle;
+        private int mTexCoordHandle;
+        private int mTexSamplerHandle;
+        private int mTexName;
 
-        MyRenderer() {
+        MyRenderer(final Context context) {
+            mContext = context;
             mVertexBuffer = ByteBuffer.allocateDirect(VERTEX.length * 4)
                     .order(ByteOrder.nativeOrder())
                     .asFloatBuffer()
                     .put(VERTEX);
             mVertexBuffer.position(0);
+
+            mVertexIndexBuffer = ByteBuffer.allocateDirect(VERTEX_INDEX.length * 2)
+                    .order(ByteOrder.nativeOrder())
+                    .asShortBuffer()
+                    .put(VERTEX_INDEX);
+            mVertexIndexBuffer.position(0);
+
+            mTexVertexBuffer = ByteBuffer.allocateDirect(TEX_VERTEX.length * 4)
+                    .order(ByteOrder.nativeOrder())
+                    .asFloatBuffer()
+                    .put(TEX_VERTEX);
+            mTexVertexBuffer.position(0);
         }
 
         static int loadShader(int type, String shaderCode) {
@@ -102,11 +150,35 @@ public class MainActivity extends AppCompatActivity {
             GLES20.glUseProgram(mProgram);
 
             mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+            mTexCoordHandle = GLES20.glGetAttribLocation(mProgram, "a_texCoord");
             mMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+            mTexSamplerHandle = GLES20.glGetUniformLocation(mProgram, "s_texture");
 
             GLES20.glEnableVertexAttribArray(mPositionHandle);
             GLES20.glVertexAttribPointer(mPositionHandle, 3, GLES20.GL_FLOAT, false,
                     12, mVertexBuffer);
+
+            GLES20.glEnableVertexAttribArray(mTexCoordHandle);
+            GLES20.glVertexAttribPointer(mTexCoordHandle, 2, GLES20.GL_FLOAT, false, 0,
+                    mTexVertexBuffer);
+
+            int[] texNames = new int[1];
+            GLES20.glGenTextures(1, texNames, 0);
+            mTexName = texNames[0];
+            Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                    R.drawable.p_300px);
+            GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTexName);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,
+                    GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER,
+                    GLES20.GL_LINEAR);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_S,
+                    GLES20.GL_REPEAT);
+            GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_WRAP_T,
+                    GLES20.GL_REPEAT);
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+            bitmap.recycle();
         }
 
         @Override
@@ -114,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
             GLES20.glViewport(0, 0, width, height);
 
             Matrix.perspectiveM(mMVPMatrix, 0, 45, (float) width / height, 0.1f, 100f);
-            Matrix.translateM(mMVPMatrix, 0, 0f, 0f, -2.5f);
+            Matrix.translateM(mMVPMatrix, 0, 0f, 0f, -5f);
         }
 
         @Override
@@ -122,8 +194,15 @@ public class MainActivity extends AppCompatActivity {
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
             GLES20.glUniformMatrix4fv(mMatrixHandle, 1, false, mMVPMatrix, 0);
+            GLES20.glUniform1i(mTexSamplerHandle, 0);
 
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
+            // 用 glDrawElements 来绘制，mVertexIndexBuffer 指定了顶点绘制顺序
+            GLES20.glDrawElements(GLES20.GL_TRIANGLES, VERTEX_INDEX.length,
+                    GLES20.GL_UNSIGNED_SHORT, mVertexIndexBuffer);
+        }
+
+        void destroy() {
+            GLES20.glDeleteTextures(1, new int[] { mTexName }, 0);
         }
     }
 }
